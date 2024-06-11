5c3bb029ad870182280d4a54843482d3fbde5ece
Thomas Wecker <thomas.wecker@icloud.com>
2024-06-11 23:31:57 +0200

    Github pages prep

---
+++ docs/Component-dbg.js
@@ -0,0 +1,39 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"./model/models",
+	"sap/ui/core/routing/History",
+	"sap/ui/Device",
+	"sap/ui/model/resource/ResourceModel"
+], function(UIComponent, models, History, Device) {
+	"use strict";
+
+	return UIComponent.extend("com.esweckert.profile.Component", {
+		metadata: {
+			manifest: "json",
+			interfaces: ["sap.ui.core.IAsyncContentCreation"]
+		},
+
+		init: function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		}
+
+
+//		getContentDensityClass: function () {
+//			if (!this._sContentDensityClass) {
+//				if (!Device.support.touch){
+//					this._sContentDensityClass = "sapUiSizeCompact";
+//				} else {
+//					this._sContentDensityClass = "sapUiSizeCozy";
+//				}
+//			}
+//			return this._sContentDensityClass;
+//		}
+	});
+});
---
+++ docs/controller/NotFound.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["./BaseController"],function(e){"use strict";return e.extend("com.esweckert.profile.controller.NotFound",{onInit:function(){},onToHome:function(){this.getOwnerComponent().getTargets().display("home")}})});
+//# sourceMappingURL=NotFound.controller.js.map
